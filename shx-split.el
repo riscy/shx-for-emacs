f67455fb37b7c1f6be9fea59b4de7c9067af9c58
riscy <chris@dissolve.com>
2017-04-22 21:22:42 -0600

    re-license as GPL 3

--- shx-split.el
+++ shx-split.el
@@ -3,9 +3,22 @@
 ;; Authors: Chris Rayner (dchrisrayner @ gmail)
 ;; Created: May 23 2011
 ;; Keywords: comint-mode, shell-mode
-;; Git: github.com/riscy/shx-for-emacs
+;; Git: https://github.com/riscy/shx-for-emacs
 
 ;; This file is NOT part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
