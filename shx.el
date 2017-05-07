2593289a7d587342ff8f0fc1a85f718560943306
riscy <chris@dissolve.com>
2017-05-06 22:35:43 -0600

    fix redundant filename expansion

--- shx.el
+++ shx.el
@@ -769,8 +769,7 @@ See `Man-notify-method' for what happens when the page is ready."
   (if (equal file "")
       (find-file-other-window "")
     (find-file-other-window
-     (let ((name (expand-file-name (car (shx--parse-filenames file)))))
-       (expand-file-name (replace-regexp-in-string "\\\\" "" file))))))
+     (expand-file-name (car (shx--parse-filenames file))))))
 
 (defun shx-cmd-pwd (_args)
   "(SAFE) Show what Emacs thinks the default directory is.
