ca2215aab5dd30c1beb3d5712b32083dc15a2b78
riscy <chris@dissolve.com>
2017-05-22 12:47:07 -0600

    update README; bump to version 0.0.1

--- shx.el
+++ shx.el
@@ -5,7 +5,7 @@
 ;; Keywords: processes, tools
 ;; Homepage: https://github.com/riscy/shx-for-emacs
 ;; Package-Requires: ((emacs "24.4"))
-;; Version: 0.0.0
+;; Version: 0.0.1
 
 ;; This file is NOT part of GNU Emacs.
 
